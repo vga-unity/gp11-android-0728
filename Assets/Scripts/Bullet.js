973daa8810e33dd7d96b47b32c79e18b0cd5613b
ryuji funo <negisenn531@yahoo.co.jp>
2012-07-28 12:48:32 +0900

    ぶよぶよ弾と体力、死亡判定、復活を追加

--- Assets/Scripts/Bullet.js
+++ Assets/Scripts/Bullet.js
@@ -2,11 +2,19 @@
 
 var speed : float;
 
+
 function Start() {
     yield WaitForSeconds(1.0);
     Destroy(gameObject);
 }
 
 function Update() {
+
+	transform.localPosition.x +=
+    	transform.right.x * Random.Range(-6.0, 6.0) * Time.deltaTime;
+    	
+    transform.localPosition.y +=
+    	transform.up.y * Random.Range(-2.0, 2.0) * Time.deltaTime;
+    	
     transform.localPosition += transform.forward * speed * Time.deltaTime;
 }
